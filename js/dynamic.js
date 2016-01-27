325999641c93c67c6affdfe0cedf8bbba37dd148
Alexandr <justdealman@gmail.com>
2016-01-27 19:57:15 +0700

    wave 2

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function() {
+﻿$(function() {
 	$('.menu > li > ul').parent().hover(
 		function() {
 			$(this).children('ul').stop(true,true).fadeIn(100);
@@ -86,6 +86,38 @@
 	if ( $('input[type="checkbox"]').length > 0 ) {
 		$('input[type="checkbox"]').uniform();
 	}
+	if ( $('input[type="radio"]').length > 0 ) {
+		$('input[type="radio"]').uniform();
+	}
+	if ( $('.product-s').length > 0 ) {
+		$('.catalog-c').append('<span class="bg-p"></span>');
+		$('.bg-p').css({
+			'height': $('.catalog-c').outerHeight()-$('.product-s').outerHeight()-26+'px'
+		});
+	}
+	if ( $('.list-c').length > 0 && $('.phone-i').length ) {
+		$('.list-c').css({
+			'padding': '0 0 94px'
+		});
+	}
+	if ( $('.list-c .more button').length > 0 ) {
+		$('.list-c .more').has('button').css({
+			'padding-bottom': '42px'
+		});
+	}
+	$('button[data-map]').bind('click', function(e) {
+		e.preventDefault();
+		var t = $('div[data-map="'+$(this).attr('data-map')+'"]');
+		if ( t.is(':visible') ) {
+			t.stop().fadeOut(100);
+			$(this).removeClass();
+		} else {
+			t.css({
+				'top': $(this).offset().top+$(this).outerHeight()+'px'
+			}).stop().fadeIn(100);
+			$(this).addClass('opened');
+		}
+	});
 	$('input, textarea').each(function() {
 		$(this).data('holder', $(this).attr('placeholder'));
 		$(this).focusin(function() {
@@ -94,6 +126,55 @@
 		$(this).focusout(function() {
 			$(this).attr('placeholder', $(this).data('holder'));
 		});
+	});
+	function searchScroll() {
+		var h = -15;
+		for ( var i=1; i<=4; i++ ) {
+			h = h+ $('.search-r ul li:nth-child('+i+')').outerHeight();
+		}
+		$('.search-r .container').css({
+			'height': h+'px'
+		});
+		$('.search-r ul').css({
+			'margin-right': '43px'
+		});
+		$('.search-r .container').jScrollPane({
+			autoReinitialise: true,
+			verticalDragMinHeight: 83,
+			verticalDragMaxHeight: 83
+		});
+	}
+	$('html').click(function(e) {
+		e.stopPropagation();
+		$('div[data-map').stop().fadeOut(100);
+		$('button[data-map]').removeClass();
+		$('.search-b').removeClass('drop');
+		$('.search-r').stop().fadeOut(100);
+	});
+	$('[data-map], .search-b').click(function(e) {
+		e.stopPropagation();
+	});
+	$('.search-b input').keyup(function() {
+		if ( $('.search-b input').val().length > 0 ) {
+			$(this).parent().addClass('drop');
+			$('.search-r').css({
+				'top': $(this).parent().offset().top+'px'
+			}).stop().fadeIn(100);
+			if ( $('.search-r ul li').length > 4 ) {
+				searchScroll();
+			}
+		} else {
+			$(this).parent().removeClass('drop');
+			$('.search-r').stop().fadeOut(100);
+		}
+	});
+	$('.search-b input').focusin(function() {
+		if ( $('.search-b input').val().length > 0 && $('.search-r ul li').length > 0 ) {
+			$(this).parent().addClass('drop');
+			$('.search-r').css({
+				'top': $(this).parent().offset().top+'px'
+			}).stop().fadeIn(100);
+		}
 	});
 });
 $(window).load(function() {
@@ -111,6 +192,19 @@ $(window).load(function() {
 			$('.menu > li > ul > li').append('<span></span>');
 			$('.menu > li > ul > li > span').each(function() {
 				$(this).height($(this).parent().parent().height()-26);
+			});
+		});
+	}
+	if ( $('.list-c > ul > li .payment').length > 0 ) {
+		$(function() {
+			var max = 0;
+			$('.list-c > ul > li .payment li').each(function() {
+				var h = $(this).height(); 
+				max = h > max ? h : max;
+			});
+			$('.list-c > ul > li .payment li').height(max);
+			$('.list-c > ul > li .payment').css({
+				'opacity': '1'
 			});
 		});
 	}
--- js/ie8.js
+++ js/ie8.js
@@ -2,44 +2,49 @@
 	$('header nav > ul > li:last-child > a').css({
 		'padding-right': '6px'
 	});
-	$('header nav > ul > li > ul  > li:nth-child(2) > a span').css({
+	$('header nav > ul > li > ul  > li:nth-child(2) > a span, .menu > li:nth-child(2) > a span, .menu > li:nth-child(2) > div span').css({
 		'border': '3px solid #617c0b'
 	});
-	$('header nav > ul > li > ul  > li:nth-child(3) > a span').css({
+	$('header nav > ul > li > ul  > li:nth-child(3) > a span, .menu > li:nth-child(3) > a span, .menu > li:nth-child(3) > div span').css({
 		'border': '3px solid #452d2d'
 	});
-	$('header nav > ul > li > ul  > li:nth-child(4) > a span').css({
+	$('header nav > ul > li > ul  > li:nth-child(4) > a span, .menu > li:nth-child(4) > a span, .menu > li:nth-child(4) > div span').css({
 		'border': '3px solid #a65e5e'
 	});
-	$('header nav > ul > li > ul  > li:nth-child(5) > a span').css({
+	$('header nav > ul > li > ul  > li:nth-child(5) > a span, .menu > li:nth-child(5) > a span, .menu > li:nth-child(5) > div span').css({
 		'border': '3px solid #758da7'
 	});
-	$('.menu > li:nth-child(2) > a span, .menu > li:nth-child(2) > div span').css({
-		'border': '3px solid #617c0b'
-	});
-	$('.menu > li:nth-child(3) > a span, .menu > li:nth-child(3) > div span').css({
-		'border': '3px solid #452d2d'
-	});
-	$('.menu > li:nth-child(4) > a span, .menu > li:nth-child(4) > div span').css({
-		'border': '3px solid #a65e5e'
-	});
-	$('.menu > li:nth-child(5) > a span, .menu > li:nth-child(5) > div span').css({
-		'border': '3px solid #758da7'
-	});
-	$('.tab-c li:last-child a').css({
-		'border-right-width': '2px',
-		'border-radius': '0 21px 21px 0'
-	});
-	$('.index .cat-b li:nth-child(4n)').css({
-		'margin-right': '0'
+	$('.tab-c li:last-child a, .private-c .nav li:last-child a').css({
+		'border-right-width': '2px'
 	});
 	$('.benefits-b .tabs > ul li:last-child a').css({
 		'border-left-width': '2px'
 	});
-	$('.advantage-b ul li:nth-child(4n), .catalog-c .rc .list li:nth-child(3n)').css({
+	$('.index .cat-b li:nth-child(4n)').css({
+		'margin-right': '0'
+	});
+	$('.advantage-b ul li:nth-child(4n), .catalog-c .rc .list li:nth-child(3n), .same-b ul li:nth-child(3n)').css({
 		'margin-right': '-20px'
 	});
 	$('.catalog-c .lc .filter .group:nth-child(2n+1)').css({
 		'background': '#ac9c94'
+	});
+	$('.table-b ul li:nth-child(1)').css({
+		'width': '553px'
+	});
+	$('.table-b table td:nth-child(1)').css({
+		'width': '70px'
+	});
+	$('.table-b ul li:nth-child(3), .table-b table td:nth-child(4)').css({
+		'width': '101px'
+	});
+	$('.table-b ul li:nth-child(2), .table-b table td:nth-child(3), .table-b ul li:nth-child(4), .table-b table td:nth-child(5)').css({
+		'width': '83px'
+	});
+	$('.payment-c > ul > li:nth-child(2n)').css({
+		'background': 'url("../img/bg_pattern.png") repeat top'
+	});
+	$('.search-r ul li:last-child').css({
+		'padding-bottom': '0'
 	});
 });
